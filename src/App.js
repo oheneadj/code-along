afc2323f9f774c8ba605edf5cb739756d4d7e6d6
oheneadj <oheneadjei.dev@gmail.com>
2022-03-28 14:33:30 +0000

    react component

--- src/App.js
+++ src/App.js
@@ -3,64 +3,33 @@ import './App.css';
 // import writers from './writers.json'
 import  ProfileCard from './ProfileCard';
 import {useEffect, useState } from "react";
+import ProfileForm from './components/ProfileForm';
 // import {useState} from "react";
 
 function App() {
-  const [ data, setData] = useState({
-    writers:[],
-    loading:false,
-  });
-
-
-
-  const handleClick = () => {
-    setData(prevData => ({
-      ...prevData,
-      loading:true,
-    }));
-    
-    setTimeout(() => {
-      const getWriters = async () => { 
-        const response = await fetch("/writers.json");
-        const data = await response.json();
-        setData({
-          writers:data,
-        loading:false,
-      });
-    };
-    getWriters();
-  }, 2000)
-};
-
-  if (data.loading){
-    return(
-      <div>
-        <h1>Writer Profiles</h1>
-        <div className="container">
-          <div className="card action">
-            <p className="infoText">Loading...</p>
-          </div>
-        </div>
-      </div>
-    )
+const [allProfile, setAllProfile] = useState([
+  {
+    firstName: "h",
+    lastName: "n",
+    email: "n@n.ocm",
+    phone: "12",
   }
+]);
+
+const submit = (profile)=> {
+  const arr = allProfile;
+  arr.push(profile);
+  setAllProfile(arr);
+}
 
   return (
     <div>
       <h1>Writer Profiles</h1>
       <div className="container">
-        {data.writers.length === 0 ? (
-          <div className="card action">
-            <p className="infoText"> Oops... no writer profile was found</p>
-            <button className="actionBtn" onClick={handleClick}>
-              Get Writers
-            </button>
-          </div>
-        ):(
-          data.writers.map((writer) => (
+        <ProfileForm />
+          {allProfile.map((writer) => (
             <ProfileCard key={writer.id} writer={writer} />
-          ))
-        )}
+          ))}
       </div>
     </div>
   );
